f5d34416ef6859e56d748c41e044802663eb135a
Kamil <kdnuriev@gmail.com>
2025-12-20 18:59:23 +0400

    fixed tab jumping

--- lua/plugins/go_ide.lua
+++ lua/plugins/go_ide.lua
@@ -67,8 +67,6 @@ return {
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
             else
               fallback()
             end
@@ -76,7 +74,19 @@ return {
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-j>"] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-k>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
